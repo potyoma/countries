221051f19ee23d8daaab538b56eb655db9ff6f6b
Artyom Novoselov <an@versta24.ru>
2023-10-06 03:28:45 +0400

    feat: add dropdown to dearch form

--- components/organisms/search-form/search-form.js
+++ components/organisms/search-form/search-form.js
@@ -4,15 +4,31 @@ import Input from "@/components/atoms/input";
 import s from "./search-form.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Dropdown from "@/components/atoms/dropdown";
+import { useState } from "react";
 
 export default function SearchForm({}) {
+  const [filter, setFilter] = useState();
+
   const handleSubmit = e => e.preventDefault();
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={s.form}>
       <Input
         icon={<FontAwesomeIcon icon={faMagnifyingGlass} />}
         placeholder="Search for a country..."
+      />
+      <Dropdown
+        options={[
+          { name: "Africa", value: "africa" },
+          { name: "America", value: "america" },
+          { name: "Asia", value: "asia" },
+          { name: "Oceania", value: "oceania" },
+          { name: "Europe", value: "europe" },
+        ]}
+        label={"Filter by Region"}
+        onSelect={setFilter}
+        selected={filter}
       />
     </form>
   );
