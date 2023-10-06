c26924a33f335423a729a6f5077902caceff81cb
Artyom Novoselov <an@versta24.ru>
2023-10-07 01:45:12 +0400

    feat: country page

--- app/country/[slug]/page.js
+++ app/country/[slug]/page.js
@@ -2,15 +2,33 @@ import { getCountry } from "@/lib/data/get-country";
 import s from "./country.module.css";
 import BackButton from "@/components/molecules/back-button/back-button";
 import Image from "next/image";
+import { Heading } from "@/components/atoms/heading";
+import Properties from "@/components/molecules/properties/properties";
+import { extractProperties } from "@/lib/helpers";
+import ListProperty from "@/components/molecules/list-property/list-property";
+
+const MAIN_PROPERTIES = [
+  "native name",
+  "population",
+  "region",
+  "sub region",
+  "capital",
+];
+const ADDITIONAL_PROPERTIES = ["top level doamin", "currencies", "languages"];
+
+const selectProperties = (properties, filter) =>
+  properties.filter(({ name }) => filter.includes(name.toLowerCase()));
 
 export default async function Page({ params }) {
   const country = await getCountry(params.slug);
 
   if (!country) return <div>{`notFound`}</div>;
 
+  const properties = extractProperties(country);
+
   return (
     <div className={s.container}>
-      <BackButton />
+      <BackButton className={s.button} />
       <Image
         src={country.flags.svg}
         width="0"
@@ -18,7 +36,20 @@ export default async function Page({ params }) {
         className={s.flag}
         alt={country.flags.alt}
       />
-      {JSON.stringify(country)}
+      <div className={s.info}>
+        <Heading className={s.heading} level="h2">
+          {country.name.common}
+        </Heading>
+        <div className={s.properties}>
+          <Properties
+            properties={selectProperties(properties, MAIN_PROPERTIES)}
+          />
+          <Properties
+            properties={selectProperties(properties, ADDITIONAL_PROPERTIES)}
+          />
+          <ListProperty name="Borders" values={country.borders} />
+        </div>
+      </div>
     </div>
   );
 }
